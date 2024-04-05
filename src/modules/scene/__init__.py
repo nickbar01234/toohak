222b82c832c7b31b0afd4c0f18241f9fbb9e27b8
nickbar01234 <nickbar01234@gmail.com>
2024-04-04 23:18:08 -0400

    Remove * import
    Co-authored-by: Tony Huang <tonyhuang12138@users.noreply.github.com>

--- src/modules/scene/__init__.py
+++ src/modules/scene/__init__.py
@@ -3,4 +3,4 @@ from .entry_scene import EntryScene
 from .quit_scene import QuitScene
 from .scene_state import SceneState
 from .question_scene import QuestionScene
-from .styles import *
+from .styles import STYLE
