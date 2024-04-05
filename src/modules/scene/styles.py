d7d2836547d2db0cd3ec63858166f8ea16c4309b
nickbar01234 <nickbar01234@gmail.com>
2024-04-04 23:02:15 -0400

    Add styling constants
    Co-authored-by: Tony Huang <tonyhuang12138@users.noreply.github.com>

---
+++ src/modules/scene/styles.py
@@ -0,0 +1,42 @@
+from typing import Literal, TypedDict
+import pygame
+
+# pylint: disable=E1101
+pygame.init()
+
+BASE = "./static/font"
+
+
+def font_builder(size: int, sty: Literal["default", "black", "bold", "extrabold"] = "default"):
+    path = {
+        "default": f"{BASE}/Montserrat-VariableFont_wght.ttf",
+        "black": f"{BASE}/Montserrat-Black.ttf",
+        "bold": f"{BASE}/Montserrat-Bold.ttf",
+        "extrabold": f"{BASE}/Montserrat-ExtraBold.ttf"
+    }
+    return pygame.font.Font(path[sty], size)
+
+
+class FontType(TypedDict):
+    title: pygame.font.Font
+    question: pygame.font.Font
+    answer: pygame.font.Font
+    text: pygame.font.Font
+
+
+class StyleType(TypedDict):
+    width: int
+    height: int
+    font: FontType
+
+
+STYLE: StyleType = {
+    "font": {
+        "title": font_builder(72, "extrabold"),
+        "question": font_builder(36, "bold"),
+        "answer": font_builder(24, "extrabold"),
+        "text": font_builder(16)
+    },
+    "width": 720,
+    "height": 1280
+}
